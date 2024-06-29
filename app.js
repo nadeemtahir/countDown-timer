5723c6388b16459ae1c0d61bfcfb4e04064ae061
manahilnadeem <163245288+nadeemtahir@users.noreply.github.com>
2024-06-29 08:44:20 -0700

    Done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#! usr/bin/env node
 import inquirer from "inquirer";
 import { differenceInSeconds } from "date-fns";
 const res = await inquirer.prompt([
@@ -15,23 +16,23 @@ const res = await inquirer.prompt([
             else {
                 return true;
             }
-        }
-    }
+        },
+    },
 ]);
 let input = res.userInput;
 function startTime(val) {
     const intTime = new Date().setSeconds(new Date().getSeconds() + val);
     const intervalTime = new Date(intTime);
-    setInterval((() => {
+    setInterval(() => {
         const currTime = new Date();
         const timeDiff = differenceInSeconds(intervalTime, currTime);
         if (timeDiff <= 0) {
             console.log("Timer has expired");
             process.exit();
         }
-        const min = Math.floor((timeDiff % (3600 * 24) / 3600));
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
         const sec = Math.floor(timeDiff % 60);
         console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
-    }), 1000);
+    }, 1000);
 }
 startTime(input);
